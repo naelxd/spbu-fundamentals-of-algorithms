8c6e87dd9ac8a4773832364110b0acfc367d853b
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2023-04-01 09:57:26 +0300

    Solved practicum 3

--- practicum_3/homework/mst.py
+++ practicum_3/homework/mst.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
 
 from src.plotting import plot_graph
 
@@ -11,9 +12,27 @@ def prim_mst(G: nx.Graph, start_node="0") -> set[tuple[Any, Any]]:
     rest_set = set(G.nodes())  # set of nodes not yet included into MST
     mst_edges = set()  # set of edges constituting MST
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    mst_set.add(start_node)
+    rest_set.remove(start_node)
+
+    while rest_set:
+        best_edge = {
+            "edge": (None, None),
+            "weight": np.inf
+        }
+        
+        new_node = None
+        for node in mst_set:
+            for node_neighbor in G.neighbors(node):
+                if (not node_neighbor in mst_set and 
+                    G.edges[node, node_neighbor]["weight"] < best_edge["weight"]):
+                    best_edge["edge"] = (node, node_neighbor)
+                    best_edge["weight"] = G.edges[node, node_neighbor]["weight"]
+                    new_node = node_neighbor
+
+        mst_set.add(new_node)
+        rest_set.remove(new_node)
+        mst_edges.add(best_edge["edge"])
 
     return mst_edges
 
--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -1,16 +1,37 @@
 from typing import Any
 
 import networkx as nx
+import numpy as np
 
 from src.plotting import plot_graph
 
 
 def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
     shortest_paths = {}  # key = destination node, value = list of intermediate nodes
+    distances = {}
+    nodes_to_visit = []
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    for node in G.nodes():
+        distances[node] = np.inf
+        shortest_paths[node] = ["0"]
+        nodes_to_visit.append(node)
+
+    distances[source_node] = 0
+
+    while nodes_to_visit:
+        # Getting node to visit with minimal distance
+        curr_node = nodes_to_visit[0]
+        for node in nodes_to_visit:
+            if distances[node] < distances[curr_node]:
+                curr_node = node
+
+        nodes_to_visit.remove(curr_node)
+
+        for neighbor in G.neighbors(curr_node):
+            dist = distances[curr_node] + G[curr_node][neighbor]["weight"]
+            if dist < distances[neighbor]:
+                distances[neighbor] = dist
+                shortest_paths[neighbor] = shortest_paths[curr_node] + [neighbor]
 
     return shortest_paths
 
