6a1858f6078217b5cd9b9e1658eed5dd2d32f634
Nikita Osovskiy <nikitaosovskiy2005ok181@gmail.com>
2023-04-01 10:46:14 +0300

    solved practicum 4

--- practicum_4/homework/time_needed_to_buy_tickets.py
+++ practicum_4/homework/time_needed_to_buy_tickets.py
@@ -2,14 +2,24 @@ from typing import Any
 
 import yaml
 import numpy as np
+from queue import Queue
 
 
 def time_taken(tickets: list[int], k: int) -> int:
     seconds_elapsed = 0
+    q = Queue()
+    for i in range(len(tickets)):
+        q.put((tickets[i], i))
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    while not q.empty():
+        el, pos = q.get()
+        if el - 1 > 0:
+            q.put((el - 1, pos))
+
+        seconds_elapsed += 1
+
+        if el - 1 == 0 and pos == k:
+            return seconds_elapsed
 
     return seconds_elapsed
 
